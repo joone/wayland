ddb078ae61cc703d543bfbf8fd0c7e3b2379c022
Joone Hur <joone.hur@intel.com>
2014-07-02 17:36:18 -0700

    updated

--- build/build_wayland.sh
+++ build/build_wayland.sh
@@ -26,7 +26,7 @@ export MAKE_COMMAND="make -j4"
 ###########################################################
 
 ###########################################################
-sudo rm -rf $WLD
+rm -rf $WLD
 mkdir $WLD
 mkdir $WLD/lib
 mkdir $WLD/lib/pkgconfig
@@ -39,12 +39,12 @@ mkdir $WLD/share/aclocal
 cd $SRC
 echo "wayland lib pull & build"
 cd wayland
-git clean -dfx
-git reset --hard
-git pull
+#git clean -dfx
+#git reset --hard
+#git pull
 ./autogen.sh --prefix=$WLD
 make
-sudo make install
+make install
 cd ..
 ###########################################################
 
@@ -61,92 +61,92 @@ cd ..
 export ACLOCAL 
 echo "Mesa pull & build"
 cd drm
-git clean -dfx
-git reset --hard
-git pull
+#git clean -dfx
+#git reset --hard
+#git pull
 ./autogen.sh --prefix=$WLD
 make
-sudo make install
+make install
 cd ..
 cd mesa
-git clean -dfx
-git reset --hard
-git pull
-./autogen.sh --prefix=$WLD --enable-gles2 --disable-gallium-egl --with-egl-platforms=x11,wayland,drm --enable-gbm --enable-shared-glapi --with-gallium-drivers=r300,r600,swrast,nouveau
+#git clean -dfx
+#git reset --hard
+#git pull
+./autogen.sh --prefix=$WLD --enable-gles2 --disable-gallium-egl --with-egl-platforms=x11,wayland,drm --enable-gbm --enable-shared-glapi --with-gallium-drivers=r300,r600,swrast,nouveau  --disable-dri3
 make
-sudo make install
+make install
 cd ..
 ###########################################################
 
 ###########################################################
 echo "libxkbcommon pull & build"
 cd libxkbcommon/
-git clean -dfx
-git reset --hard
-git pull
-./autogen.sh --prefix=$WLD --with-xkb-config-root=/usr/share/X11/xkb
+#git clean -dfx
+#git reset --hard
+#git pull
+./autogen.sh --prefix=$WLD --with-xkb-config-root=/usr/share/X11/xkb --disable-x11
 make
-sudo make install
+make install
 cd ..
 ###########################################################
 
 ###########################################################
 echo "cairo-gl pull & build"
 cd pixman
-git clean -dfx
-git reset --hard
-git pull
+#git clean -dfx
+#git reset --hard
+#git pull
 ./autogen.sh --prefix=$WLD
 make
-sudo make install
+make install
 cd ..
 cd cairo
-git clean -dfx
-git reset --hard
-git pull
+#git clean -dfx
+#git reset --hard
+#git pull
 ./autogen.sh --prefix=$WLD --enable-gl --enable-xcb
 make
-sudo make install
+make install
 cd ..
 ###########################################################
 
 ###########################################################
 echo "libunwind pull & build"
 cd libunwind
-git clean -dfx
-git reset --hard
-git pull
+#git clean -dfx
+#git reset --hard
+#git pull
 autoreconf -i
 ./configure --prefix=$WLD
 make
-sudo make install
+make install
 cd ..
 ###########################################################
 
 ###########################################################
-echo "colord clon & build"
-cd colord
-git clean -dfx
-git reset --hard
-git checkout master
-git pull
-git checkout COLORD_0_1_27
-./autogen.sh --prefix=$WLD --disable-static --disable-gtk-doc --disable-gusb --disable-vala
-make
-sudo make install
-cd ..
+#echo "colord clon & build"
+#cd colord
+#git clean -dfx
+#git reset --hard
+#git checkout master
+#git pull
+#git checkout COLORD_0_1_27
+#./autogen.sh --prefix=$WLD --disable-static --disable-gtk-doc --disable-gusb --disable-vala
+#make
+#make install
+#cd ..
 ###########################################################
 
 ###########################################################
 echo "weston pull & build"
 cd weston
-git clean -dfx
-git reset --hard
-git pull
+#git clean -dfx
+#git reset --hard
+#git pull
 #./autogen.sh --prefix=$WLD --disable-colord
 ./autogen.sh --prefix=$WLD 
 make
-sudo make install
+make install
 cd ..
 ###########################################################
 
